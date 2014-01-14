9385f12760cb6b24812bfc45195fed65db4f6564
gomez <gomez@bd53911d-e828-0410-8381-a983918e8cd0>
2014-01-14 22:24:14 +0000

    J

--- src/LinkDef.h
+++ src/LinkDef.h
@@ -20,6 +20,6 @@
 #pragma link C++ class irene::Event+;
 #pragma link C++ class irene::ParameterInfo+;
 #pragma link C++ class std::pair<TLorentzVector,double>+;
-#pragma link C++ class std::vector<std::pair<TLorentzVector,double>*>+;
+#pragma link C++ class std::vector<std::pair<TLorentzVector,double>>+;
 
 #endif
